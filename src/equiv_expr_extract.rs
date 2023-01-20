048a1cabd7deab35d3f233f43a2d06f57eb9f64e
Hongbo Zheng <hongboz2@illinois.edu>
2023-01-20 00:15:09 -0600

    replace regex with distinct replace

--- src/equiv_expr_extract.rs
+++ src/equiv_expr_extract.rs
@@ -116,13 +116,32 @@ impl ContextGrammar {
     /// * `self`
     pub fn get_rw(&self) -> Vec<String> { return self.rw.clone(); }
 
-    /// ## member function to extract all equivalent mathematical expressions
+    /// ## private member function to replace distinct eclass with rewrite rule
+    /// ## Argument
+    /// * `self`
+    /// * `op`  - operand that needs to be replaced
+    /// * `rw`  - rewrite rule that is going to be replaced with
+    /// * `str` - original expression
+    fn distinct_replace(&self, op: &str, rw: String, str: &mut String) {
+        let matches: Vec<_> = str.match_indices(op).collect();
+        for mat in matches {
+            let start_idx = mat.0;
+            let end_idx = start_idx + op.len();
+            if (end_idx != str.len() && str.chars().nth(end_idx).unwrap() == ' ') ||
+                end_idx == str.len() {
+                str.replace_range(start_idx..end_idx, &*rw);
+                break;
+            }
+        }
+    }
+
+    /// ## private member function to extract all equivalent mathematical expressions
     /// ## Context-Sensitive Grammar
     /// ## Argument
     /// * `self`
     /// * `str` - rewrite expression
     /// * `idx` - fn call idx for debugging purpose
-    pub fn csg_extract(&mut self, mut str: String, idx: u8) {
+    fn csg_extract(&mut self, mut str: String, idx: u8) {
         if self.DEBUG { println!("-----------------------------------"); }
         if self.DEBUG { println!("[DEBUG]: Function Call {}", idx); }
         let str_tmp = str.clone();
@@ -154,8 +173,20 @@ impl ContextGrammar {
                 let rw = rw_list[k].clone();
                 if self.DEBUG { println!("[SSTR]:  {}", str); }
                 if self.DEBUG { println!("[ RW ]:  {}", rw); }
+                /**
+                Regex will solve indistinct eclass match in str.replacen()
+                Original Code
+                ```
+                str = str.replacen(op, &*rw, 1);
+                ```
+                Using Regex (has performance issue since it's slow)
+                ```
+                # use regex::Regex;
                 let mat = Regex::new(format!(r"\b{}\b", op).as_str()).unwrap().find(str.as_str()).unwrap();
                 str.replace_range(mat.start()..mat.end(), &rw);
+                ```
+                 */
+                self.distinct_replace(op, rw, &mut str);
                 if self.DEBUG { println!("[AFTER]: {}", str); }
 
                 if str.len() >= self.max_rw_len as usize {
@@ -184,13 +215,13 @@ impl ContextGrammar {
         if self.DEBUG { println!("-----------------------------------"); }
     }
 
-    /// ## member function to extract all equivalent mathematical expressions
+    /// ## private member function to extract all equivalent mathematical expressions
     /// ## Context-Free Grammar
     /// ## Argument
     /// * `self`
     /// * `str` - rewrite expression
     /// * `idx` - fn call idx for debugging purpose
-    pub fn cfg_extract(&mut self, mut str: String, idx: u8) {
+    fn cfg_extract(&mut self, mut str: String, idx: u8) {
         if self.DEBUG { println!("-----------------------------------"); }
         if self.DEBUG { println!("[DEBUG]: Function Call {}", idx); }
         let str_tmp = str.clone();
@@ -222,8 +253,20 @@ impl ContextGrammar {
                 let rw = rw_list[k].clone();
                 if self.DEBUG { println!("[SSTR]:  {}", str); }
                 if self.DEBUG { println!("[ RW ]:  {}", rw); }
+                /**
+                Regex will solve indistinct eclass match in str.replacen()
+                Original Code
+                ```
+                str = str.replacen(op, &*rw, 1);
+                ```
+                Using Regex (has performance issue since it's slow)
+                ```
+                # use regex::Regex;
                 let mat = Regex::new(format!(r"\b{}\b", op).as_str()).unwrap().find(str.as_str()).unwrap();
                 str.replace_range(mat.start()..mat.end(), &rw);
+                ```
+                 */
+                self.distinct_replace(op, rw, &mut str);
                 if self.DEBUG { println!("[AFTER]: {}", str); }
 
                 if str.len() >= self.max_rw_len as usize {
