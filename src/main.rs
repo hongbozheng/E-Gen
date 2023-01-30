0bafc6a026d9ada6bfe8f1a969617cb47ff44b7c
Hongbo Zheng <hongboz2@illinois.edu>
2023-01-30 14:32:11 -0600

    call fn in utils.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{process::exit};
-use egg::{ContextGrammar, Language, Extractor, AstSize};
-use regex::Regex;
+use env_logger::init;
+use egg::{ContextGrammar, Language, Extractor, AstSize, pt_egraph_info, pt_root_ecls_info, pt_grammar, pt_init_rw, EquivalentExtract};
 
 fn help() {
     println!("[USAGE]: cargo run -csg <csg flag> -de <debug flag> -len <max rw len>");
@@ -115,88 +115,62 @@ pub fn main() {
     // let init_expr: &str = "(* (cos x) y)";
     // let init_expr: &str = "(/ (d x (sin x)) (* -1 (d x (cos x))))";
     // let init_expr: &str = "(* (sin x) y)";
-    let init_expr: &str = "(/ (d x (* x x)) 2)";
+    // let init_expr: &str = "(/ (d x (* x x)) 2)";
     // let init_expr: &str = "(/ (* 1 x) 1)";
-    let init_expr: &str = "(/ (* 2 x) (* 2 x))";
+    // let init_expr: &str = "(/ (* 2 x) (* 2 x))";
     // let init_expr: &str = "(/ (/ (* 2 x) 2) x)";
     // let init_expr: &str = "(/ x x)";
     /* commutative rule break extraction */
     // let init_expr: &str = "(/ (* (* (d x (sin x)) (/ 1 (cos x))) (sin x)) (* -1 (d x (cos x))))";
     println!("[INFO]: Initial expression {}", init_expr);
 
-    let mut ctx_g = ContextGrammar::new(csg, DEBUG, max_rw_len, init_expr);
+    let mut ctx_gr = ContextGrammar::new(DEBUG, init_expr);
     println!("\n[INFO]: Creating egraph with initial expression & rewrite rules...");
-    ctx_g.set_egraph();
+    ctx_gr.set_egraph();
     println!("[INFO]: Creating grammar...");
-    ctx_g.set_grammar();
+    ctx_gr.set_grammar();
     println!("[INFO]: Setting initial rewrite...");
-    ctx_g.set_init_rw();
+    ctx_gr.set_init_rw();
 
-    let egraph = ctx_g.get_egraph();
+    let egraph = ctx_gr.get_egraph();
     println!("[INFO]: EGraph total size {}", egraph.total_size());
     println!("[INFO]: EGraph contains {} node(s)", egraph.total_number_of_nodes());
     println!("[INFO]: EGraph contains {} eclass(es)", egraph.number_of_classes());
 
     /* TODO: DEBUG */
-    println!("\n[DEBUG]: ------- EGraph Information -------");
-    println!("[DEBUG]: ------------- EClass -------------");
-    for eclass in egraph.classes() {
-        println!("[DEBUG]: ------------ EClass {} ------------", eclass.id);
-        for i in 0..eclass.nodes.len() {
-            print!("[DEBUG]: enode {}", eclass.nodes[i]);
-            for k in 0..eclass.nodes[i].children().len() {
-                print!(" {}", eclass.nodes[i].children()[k]);
-            }
-            println!();
-        }
-        print!("[DEBUG]: parents");
-        for k in 0..eclass.parents().len() {
-            print!(" {:?}", eclass.parents().nth(k).unwrap());
-        }
-        println!("\n[DEBUG]: data  {:?}", eclass.data);
-    }
-    println!("[DEBUG]: ----------------------------------");
+    pt_egraph_info(&egraph);
 
-    println!("\n[INFO]: ---------- Root EClasses ----------");
-    let root_eclasses = ctx_g.get_root_eclasses();
-    print!("[INFO]:");
-    for i in 0..root_eclasses.len() {
-        print!(" {}", root_eclasses[i]);
-    }
-    println!("\n[INFO]: -----------------------------------");
+    let root_ecls = ctx_gr.get_root_ecls();
+    pt_root_ecls_info(&root_ecls);
 
     /* TODO: DEBUG */
     println!("\n[DEBUG]: ------------ Extractor -----------");
     let extractor = Extractor::new(&egraph, AstSize);
-    let (best_cost, simpl_expr) = extractor.find_best(root_eclasses[0]);
+    let (best_cost, simpl_expr) = extractor.find_best(root_ecls[0]);
     println!("[DEBUG]: Simplified Expression to {} with Cost {}",simpl_expr, best_cost);
     println!("[DEBUG]: ----------------------------------");
 
-    println!("\n[INFO]: ------------- Grammar -------------");
-    let grammar = ctx_g.get_grammar();
-    for (eclass, rewrite) in grammar {
-        println!("[INFO]: {} -> {:?}", eclass, rewrite);
-    }
-    println!("[INFO]: -----------------------------------");
+    let grammar = ctx_gr.get_grammar();
+    pt_grammar(grammar);
 
-    println!("\n[INFO]: --------- Initial Rewrite ---------");
-    let init_rw = ctx_g.get_init_rw();
-    println!("[INFO]: {:?}", init_rw);
-    println!("[INFO]: -----------------------------------");
+    let init_rw = ctx_gr.get_init_rw();
+    pt_init_rw(init_rw);
 
-    ctx_g.extract();
-    let mut rw_list = ctx_g.get_rw();
-    let orig_rw_num = rw_list.len();
-    /* rw_list.sort_unstable(); */
-    rw_list.dedup();
-    if orig_rw_num == rw_list.len() {
-        println!("[INFO]: RW are all unique");
-    } else {
-        println!("[INFO]: RW have duplicates");
-    }
-    rw_list.sort_by(|rw1, rw2| rw1.len().cmp(&rw2.len()));
-    println!("[INFO]: Total # of RW {}", rw_list.len());
-    for rw in rw_list {
-        println!("[INFO]: {}", rw);
-    }
+    let mut equiv_ext = EquivalentExtract::new(csg, DEBUG, max_rw_len, grammar.clone(), ctx_gr);
+
+    // ctx_g.extract();
+    // let mut rw_list = ctx_g.get_rw();
+    // let orig_rw_num = rw_list.len();
+    // /* rw_list.sort_unstable(); */
+    // rw_list.dedup();
+    // if orig_rw_num == rw_list.len() {
+    //     println!("[INFO]: RW are all unique");
+    // } else {
+    //     println!("[INFO]: RW have duplicates");
+    // }
+    // rw_list.sort_by(|rw1, rw2| rw1.len().cmp(&rw2.len()));
+    // println!("[INFO]: Total # of RW {}", rw_list.len());
+    // for rw in rw_list {
+    //     println!("[INFO]: {}", rw);
+    // }
 }
