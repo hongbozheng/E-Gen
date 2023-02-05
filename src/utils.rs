835ba6200063a3f66e20134bd2b9be54e30a39b9
Hongbo Zheng <hongboz2@illinois.edu>
2023-02-05 15:15:38 -0600

    add print var type & improve pt egraph info

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,12 @@
 use crate::*;
 
+/// ## function to print the type of a variable
+/// ## Argument
+/// * `_` - reference of any variable
+pub fn pt_type_of<T>(_: &T) {
+    println!("[DEBUG]: Var Type {}", std::any::type_name::<T>());
+}
+
 /// ## function to print egraph information
 /// ## Argument
 /// * `egraph` - egraph
@@ -9,9 +16,13 @@ pub fn pt_egraph_info(egraph: &MathEGraph) {
     for eclass in egraph.classes() {
         println!("[DEBUG]: ------------ EClass {} ------------", eclass.id);
         for i in 0..eclass.nodes.len() {
-            print!("[DEBUG]: enode {}", eclass.nodes[i]);
-            for k in 0..eclass.nodes[i].children().len() {
-                print!(" {}", eclass.nodes[i].children()[k]);
+            if eclass.nodes[i].to_string().parse::<f64>().is_ok() {
+                print!("[DEBUG]: <f64> {:?}", eclass.nodes[i]);
+            } else {
+                print!("[DEBUG]: enode {}", eclass.nodes[i]);
+                for k in 0..eclass.nodes[i].children().len() {
+                    print!(" {}", eclass.nodes[i].children()[k]);
+                }
             }
             println!();
         }
