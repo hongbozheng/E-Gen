98e72c8d744741911d827dbe21d4556ebb417326
Hongbo Zheng <hongboz2@illinois.edu>
2023-02-05 23:23:43 -0600

    use logger to print debug msg

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{process::exit};
-use egg::{ContextGrammar, ExpressionExtract, Language, Extractor, AstSize,
-          pt_egraph_info, pt_root_ecls_info, pt_grammar, pt_init_rw};
+use std::fmt::format;
+use egg::{ContextGrammar, ExpressionExtract, Language, Extractor, AstSize, pt_egraph_info, pt_root_ecls_info, pt_grammar, pt_init_rw, pt_skip_ecls, log_trace, log_debug, log_info, log_warn, log_error, log_fatal, log_info_raw, log_debug_raw};
 
 fn help() {
     println!("[USAGE]: cargo run -csg <csg flag> -de <debug flag> -len <max rw len>");
@@ -86,10 +86,11 @@ pub fn main() {
 
     match args.len() {
         1 => {
-            println!("[INFO]: Executing program with the following default flags...");
-            println!("[INFO]: csg flag   = false");
-            println!("[INFO]: debug flag = false");
-            println!("[INFO]: max rw len = 25\n");
+            log_info("Executing program with the following default flags...\n");
+            log_info("csg flag   = false\n");
+            log_info("debug flag = false\n");
+            log_info("max rw len = 25\n");
+            log_info_raw("\n");
         },
         2 => { help(); },
         3 => {
@@ -117,7 +118,7 @@ pub fn main() {
     // let init_expr: &str = "(+ (pow (sin x) 2) (pow (cos x) 2))";
     // let init_expr: &str = "(/ (d x (sin x)) (* -1 (d x (cos x))))";
     // let init_expr: &str = "(/ (sec x) (sin x))"; // wow...
-    let init_expr: &str = "(cos (/ x 2))";
+    let init_expr: &str = "(+ (* (cos (/ x 2)) 1) 0)";
     // let init_expr: &str = "(sqrt (/ x 2))";
     // let init_expr: &str = "(* (* x 2) 2)";
     // let init_expr: &str = "(/ 1 (sec x))";
@@ -144,20 +145,20 @@ pub fn main() {
     // let init_expr: &str = "(/ x x)";
     /* commutative rule break extraction */
     // let init_expr: &str = "(/ (* (* (d x (sin x)) (/ 1 (cos x))) (sin x)) (* -1 (d x (cos x))))";
-    println!("[INFO]: Initial expression {}", init_expr);
+    log_info(format!("Initial expression {}\n", init_expr).as_str());
 
     let mut ctx_gr = ContextGrammar::new(DEBUG, init_expr);
-    println!("\n[INFO]: Creating egraph with initial expression & rewrite rules...");
+    log_info("Creating egraph with initial expression & rewrite rules...\n");
     ctx_gr.set_egraph();
-    println!("[INFO]: Creating grammar...");
+    log_info("Creating grammar...\n");
     ctx_gr.set_grammar();
-    println!("[INFO]: Setting initial rewrite...");
+    log_info("Setting initial rewrite...\n");
     ctx_gr.set_init_rw();
 
     let egraph = ctx_gr.get_egraph();
-    println!("[INFO]: EGraph total size {}", egraph.total_size());
-    println!("[INFO]: EGraph contains {} node(s)", egraph.total_number_of_nodes());
-    println!("[INFO]: EGraph contains {} eclass(es)", egraph.number_of_classes());
+    log_info(format!("EGraph total size {}\n", egraph.total_size()).as_str());
+    log_info(format!("EGraph contains {} node(s)\n", egraph.total_number_of_nodes()).as_str());
+    log_info(format!("EGraph contains {} eclass(es)\n", egraph.number_of_classes()).as_str());
 
     /* TODO: DEBUG */
     pt_egraph_info(&egraph);
@@ -166,11 +167,15 @@ pub fn main() {
     pt_root_ecls_info(&root_ecls);
 
     /* TODO: DEBUG */
-    println!("\n[DEBUG]: ------------ Extractor -----------");
+    log_debug_raw("\n");
+    log_debug("------------ Extractor -----------\n");
     let extractor = Extractor::new(&egraph, AstSize);
     let (best_cost, simpl_expr) = extractor.find_best(root_ecls[0]);
-    println!("[DEBUG]: Simplified Expression to {} with Cost {}",simpl_expr, best_cost);
-    println!("[DEBUG]: ----------------------------------");
+    log_debug(format!("Simplified Expression to {} with Cost {}\n",simpl_expr, best_cost).as_str());
+    log_debug("----------------------------------\n");
+
+    let skip_ecls = ctx_gr.get_skip_ecls();
+    pt_skip_ecls(skip_ecls);
 
     let grammar = ctx_gr.get_grammar();
     pt_grammar(grammar);
@@ -178,23 +183,28 @@ pub fn main() {
     let init_rw = ctx_gr.get_init_rw();
     pt_init_rw(init_rw);
 
+    log_info_raw("\n");
+    log_info(format!("Total # of grammar {}\n", grammar.len()).as_str());
+
     let mut expr_ext = ExpressionExtract::new(csg, DEBUG, max_rw_len, ctx_gr);
     expr_ext.extract();
     let mut rw_list = expr_ext.get_rw().clone();
     for rw in &rw_list {
-        println!("[INFO]: {}", rw);
+        log_info(format!("{}\n", rw).as_str());
     }
     let orig_rw_num = rw_list.len();
     rw_list.sort_unstable();
     rw_list.dedup();
+    log_info_raw("\n");
     if orig_rw_num == rw_list.len() {
-        println!("\n[INFO]: RW are all unique");
+        log_info("RW are all unique\n");
     } else {
-        println!("\n[INFO]: RW have duplicates");
+        log_info("RW have duplicates\n");
     }
     rw_list.sort_by(|rw1, rw2| rw1.len().cmp(&rw2.len()));
-    println!("\n[INFO]: Total # of RW {}", rw_list.len());
+    log_info_raw("\n");
+    log_info(format!("Total # of RW {}\n", rw_list.len()).as_str());
     for rw in &rw_list {
-        println!("[INFO]: {}", rw);
+        log_info(format!("{}\n", rw).as_str());
     }
 }
