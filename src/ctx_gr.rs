c2af8604ec255ab508145dd37af55626acfd4b71
Hongbo Zheng <hongboz2@illinois.edu>
2023-02-06 01:42:01 -0600

    add skip eclass for optimization

--- src/ctx_gr.rs
+++ src/ctx_gr.rs
@@ -1,10 +1,11 @@
+use std::borrow::BorrowMut;
 use crate::*;
 
 pub struct ContextGrammar {
     init_expr: &'static str,                /* initial expression to run with egraph        */
     egraph: MathEGraph,                     /* egraph after running rewrite rules           */
     root_ecls: Vec<Id>,                     /* root eclasses of MathEGraph                  */
-    skip_ecls: Vec<String>,                 /* eclass(es) to skip during extraction         */
+    skip_ecls: HashMap<String, f64>,        /* eclass(es) to skip during extraction         */
     grammar: HashMap<String, Vec<String>>,  /* hashmap storing the grammar from egraph      */
     init_rw: Vec<String>,                   /* initial rw e.g. (* e0 e1)                    */
 }
@@ -18,7 +19,7 @@ impl ContextGrammar {
             init_expr,
             egraph: Default::default(),
             root_ecls: vec![],
-            skip_ecls: vec![],
+            skip_ecls: Default::default(),
             grammar: Default::default(),
             init_rw: vec![],
         }
@@ -48,17 +49,6 @@ impl ContextGrammar {
     /// * `self`
     pub fn get_root_ecls(&self) -> &Vec<Id> { return &self.root_ecls; }
 
-    /// ## private member function to check if a enode is NotNan<f64>
-    /// ## Argument
-    /// * `enodes` - Vec of enodes
-    fn ecls_skip(&self, enodes: &Vec<Math>) -> bool {
-        if enodes.len() == 1 && (enodes[0].to_string().parse::<f64>().unwrap() == 1.0 ||
-            enodes[0].to_string().parse::<f64>().unwrap() == 0.0) {
-            return true;
-        }
-        return false;
-    }
-
     /// ## member function to set grammar from egraph
     /// ## Argument
     /// * `self`
@@ -67,9 +57,18 @@ impl ContextGrammar {
         for eclass in eclasses {
             let mut rewrite_rules: Vec<String> = vec![];
             let id = eclass.id;
-            let ec: String = format!("{}{}", "e", id);
+            let ecls: String = format!("{}{}", "e", id);
             let enodes = &eclass.nodes;
-            // if self.ecls_skip(enodes) { self.skip_ecls.push(ec.clone()); }
+
+            if enodes.len() == 1 {
+                match enodes[0].to_string().parse::<f64>() {
+                    Ok(1.0) => { self.skip_ecls.insert(ecls.clone(), 1.0 as f64); },
+                    Ok(0.0) => { self.skip_ecls.insert(ecls.clone(), 0.0 as f64); },
+                    Ok(_) => {},
+                    Err(_) => {},
+                }
+            }
+
             for enode in enodes {
                 let mut rewrite = enode.to_string();
                 let children = enode.children();
@@ -78,14 +77,14 @@ impl ContextGrammar {
                 }
                 rewrite_rules.push(rewrite);
             }
-            self.grammar.insert(ec, rewrite_rules);
+            self.grammar.insert(ecls, rewrite_rules);
         }
     }
 
     /// ## member function to get skip_ecls from self
     /// ## Argument
     /// * `self`
-    pub fn get_skip_ecls(&self) -> &Vec<String> { return &self.skip_ecls; }
+    pub fn get_skip_ecls(&self) -> &HashMap<String, f64> { return &self.skip_ecls; }
 
     /// ## member function to get grammar from self
     /// ## Argument
@@ -96,12 +95,12 @@ impl ContextGrammar {
     /// ## Argument
     /// * `self`
     pub fn set_init_rw(&mut self) {
-        for i in 0..self.root_ecls.len() {
-            let mut root_ecls = format!("{}{}", "e", self.root_ecls[i]);
+        for rc in &self.root_ecls {
+            let mut root_ecls = format!("{}{}", "e", rc);
             if self.grammar.contains_key(&*root_ecls) {
                 self.init_rw = self.grammar.get(&*root_ecls).unwrap().clone();
             } else {
-                root_ecls = format!("{}{}", "e", self.egraph.find(self.root_ecls[i]));
+                root_ecls = format!("{}{}", "e", self.egraph.find(*rc));
                 self.init_rw = self.grammar.get(&*root_ecls).unwrap().clone();
             }
         }
