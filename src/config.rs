7322fa330276f8a027a1881ee5d6020f841e0732
Hongbo Zheng <hongboz2@illinois.edu>
2023-02-10 07:37:44 -0600

    add optimization parameters

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,9 @@
 use crate::*;
 
+/// log level for the entire environment
 pub static LOG_LEVEL: LogLevel = LogLevel::Debug;
+/// suppress meaningless rewrite rules (e.g. * 1, + 0, pow 1)
+pub static SUPPRESS: bool = true;
+/// maximum limit of rewrite rule frequency
+pub static FREQ_MAX: u8 = 1;
+/* may put max_rw_len here as global ver */
