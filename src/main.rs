a42f37a37d35c86d053703a28ca84cd177a25755
Hongbo Zheng <hongboz2@illinois.edu>
2023-03-03 20:56:58 -0600

    print total # of rw, disable egraph info print

--- src/main.rs
+++ src/main.rs
@@ -133,8 +133,8 @@ pub fn main() {
     // let init_expr: &str = "(- (pow (cos x) 2) (pow (sin x) 2))";
     // let init_expr: &str = "(/ (sin x) (cos x))";
     // let init_expr: &str = "(/ (cos x) (sec x))";
-    let init_expr: &str = "(d x (pow (cos x) 2))";
-    // let init_expr: &str = "(d x (pow (sin x) 2))";
+    // let init_expr: &str = "(d x (pow (cos x) 2))";
+    let init_expr: &str = "(d x (pow (sin x) 2))";
     // let init_expr: &str = "(- (cos x) (cos y))";
     // let init_expr: &str = "(tan (- x y))";
     // let init_expr: &str = "(sin (- x y))";
@@ -143,11 +143,16 @@ pub fn main() {
     // let init_expr: &str = "(d x (tan x))"; // wow...
 
     // let init_expr: &str = "(* (sin x) y)";
-    // let init_expr: &str = "(/ (d x (* x x)) 2)";
+    let init_expr: &str = "(/ (d x (* x x)) 2)";
+    // let init_expr: &str = "(/ (d x (* x x)) y)";
     // let init_expr: &str = "(/ (* 1 x) 1)";
     // let init_expr: &str = "(/ (* 2 x) (* 2 x))";
+    // let init_expr: &str = "(/ (* 2 x) (* 2 1))";
     // let init_expr: &str = "(/ (/ (* 2 x) 2) x)";
     // let init_expr: &str = "(/ x x)";
+    // let init_expr: &str = "(+ (csch x) (cosh x))";
+    // let init_expr: &str = "(d x (+ (pow x 2) (pow (sin x) 2)))";
+
     /* commutative rule break extraction */
     // let init_expr: &str = "(/ (* (* (d x (sin x)) (/ 1 (cos x))) (sin x)) (* -1 (d x (cos x))))";
     log_info(format!("Initial expression {}\n", init_expr).as_str());
@@ -163,7 +168,7 @@ pub fn main() {
     log_info(format!("EGraph contains {} eclass(es)\n", egraph.number_of_classes()).as_str());
 
     /* TODO: DEBUG */
-    pt_egraph_info(&egraph);
+    // pt_egraph_info(&egraph);
 
     let root_ecls = ctx_gr.get_root_ecls();
     pt_root_ecls_info(&root_ecls);
@@ -183,13 +188,15 @@ pub fn main() {
         pt_skip_ecls(skip_ecls);
 
         let grammar = get_global_grammar();
-        pt_grammar(grammar);
+        // pt_grammar(grammar);
         log_info_raw("\n");
         log_info(format!("Total # of grammar {}\n", grammar.len()).as_str());
     }
 
     let init_rw = ctx_gr.get_init_rw();
     pt_init_rw(init_rw);
+    log_info_raw("\n");
+    log_info(format!("Total # of initial rw {}\n", init_rw.len()).as_str());
     extract(csg, init_rw.clone());
 
     unsafe {
