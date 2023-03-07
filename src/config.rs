cc7e61cd782cf8cc892c72aa5d89f1b2af59e442
Hongbo Zheng <hongboz2@illinois.edu>
2023-03-07 01:43:23 -0600

    update config with default hyper-parameter

--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,16 @@
 use crate::*;
 
 /* global variables */
+/// log level for the entire environment
+pub static LOG_LEVEL: LogLevel = LogLevel::Debug;
 /// percentage of max # of OS threads
-pub static MAX_NUM_THREADS_PCT: f32 = 0.50;
+pub static mut THREAD_PCT: f32 = 1.00;
 /// max # of threads can be used (not max # of OS threads)
 pub static mut MAX_NUM_THREADS: u32 = 0;
-/// log level for the entire environment
-pub static LOG_LEVEL: LogLevel = LogLevel::Info;
 /// max rewrite str len
-pub static mut MAX_RW_LEN: u8 = 20;
+pub static mut MAX_RW_LEN: u8 = 25;
+/// context-sensitive grammar flag (csg flag)
+pub static mut CSG: bool = true;
 /// suppress meaningless rewrite rules (e.g. * 1, + 0, pow 1)
 pub static SUPPRESS: bool = true;
 /// maximum limit of rewrite rule frequency
