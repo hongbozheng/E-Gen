f9f10a8d350fbd95ca53234697d6b1e54797b528
Hongbo Zheng <hongboz2@illinois.edu>
2023-03-16 23:12:24 -0500

    use & to convert String to &str

--- src/utils.rs
+++ src/utils.rs
@@ -16,7 +16,7 @@ pub fn set_max_num_threads() {
     let max_os_threads = match max_os_threads_str.parse::<u32>() {
         Ok(max_os_threads) => max_os_threads,
         Err(e) => {
-            log_error(format!("Failed to parse {}: {}", max_os_threads_str, e).as_str());
+            log_error(&format!("Failed to parse {}: {}", max_os_threads_str, e));
             return;
         }
     };
@@ -43,7 +43,7 @@ pub unsafe fn set_csg(csg: bool) { CSG = csg; }
 /// ## Return
 /// * `None`
 pub fn pt_type_of<T>(_: &T) {
-    log_debug(format!("Var Type {}", std::any::type_name::<T>()).as_str());
+    log_debug(&format!("Var Type {}", std::any::type_name::<T>()));
 }
 
 /// ## function to print egraph information
@@ -56,24 +56,24 @@ pub fn pt_egraph_info(egraph: &MathEGraph) {
     log_debug("------- EGraph Information -------\n");
     log_debug("------------- EClass -------------\n");
     for eclass in egraph.classes() {
-        log_debug(format!("------------ EClass {} ------------\n", eclass.id).as_str());
+        log_debug(&format!("------------ EClass {} ------------\n", eclass.id));
         for i in 0..eclass.nodes.len() {
             if eclass.nodes[i].to_string().parse::<f64>().is_ok() {
-                log_debug(format!("<f64> {}", eclass.nodes[i]).as_str());
+                log_debug(&format!("<f64> {}", eclass.nodes[i]));
             } else {
-                log_debug(format!("enode {}", eclass.nodes[i]).as_str());
+                log_debug(&format!("enode {}", eclass.nodes[i]));
                 for k in 0..eclass.nodes[i].children().len() {
-                    log_debug_raw(format!(" {}", eclass.nodes[i].children()[k]).as_str());
+                    log_debug_raw(&format!(" {}", eclass.nodes[i].children()[k]));
                 }
             }
             log_debug_raw("\n");
         }
         log_debug("parents");
         for k in 0..eclass.parents().len() {
-            log_debug_raw(format!(" {:?}", eclass.parents().nth(k).unwrap()).as_str());
+            log_debug_raw(&format!(" {:?}", eclass.parents().nth(k).unwrap()));
         }
         log_debug_raw("\n");
-        log_debug(format!("data {:?}\n", eclass.data).as_str());
+        log_debug(&format!("data {:?}\n", eclass.data));
     }
     log_debug("----------------------------------\n");
 }
@@ -86,7 +86,7 @@ pub fn pt_egraph_info(egraph: &MathEGraph) {
 pub fn pt_root_ecls_info(root_ecls: &Vec<Id>) {
     log_debug_raw("\n");
     log_debug("---------- Root EClasses ----------\n");
-    log_debug(format!("{:?}\n", root_ecls).as_str());
+    log_debug(&format!("{:?}\n", root_ecls));
     log_debug("-----------------------------------\n");
 }
 
@@ -98,7 +98,7 @@ pub fn pt_root_ecls_info(root_ecls: &Vec<Id>) {
 pub fn pt_skip_ecls(skip_ecls: &HashMap<String, f64>) {
     log_debug_raw("\n");
     log_debug("---------- Skip EClasses ----------\n");
-    log_debug(format!("{:?}\n", skip_ecls).as_str());
+    log_debug(&format!("{:?}\n", skip_ecls));
     log_debug("-----------------------------------\n");
 }
 
@@ -111,7 +111,7 @@ pub fn pt_grammar(grammar: &HashMap<String, Vec<String>>){
     log_debug_raw("\n");
     log_debug("------------- Grammar -------------\n");
     for (eclass, rewrite) in grammar {
-        log_debug(format!("{} -> {:?}\n", eclass, rewrite).as_str());
+        log_debug(&format!("{} -> {:?}\n", eclass, rewrite));
     }
     log_debug("-----------------------------------\n");
 }
@@ -124,7 +124,7 @@ pub fn pt_grammar(grammar: &HashMap<String, Vec<String>>){
 pub fn pt_init_rw(init_rw: &Vec<String>) {
     log_debug_raw("\n");
     log_debug("--------- Initial Rewrite ---------\n");
-    log_debug(format!("{:?}\n", init_rw).as_str());
+    log_debug(&format!("{:?}\n", init_rw));
     log_debug("-----------------------------------\n");
 }
 
@@ -138,6 +138,6 @@ pub fn pt_rw(mutex: &Arc<Mutex<Vec<String>>>) {
     rw_vec.sort_unstable();
     rw_vec.dedup();
     for rw in rw_vec.iter() {
-        log_info(format!("{}\n", rw).as_str());
+        log_info(&format!("{}\n", rw));
     }
 }
