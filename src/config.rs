1fe8f0504152512320204726ee30faecc4ab52ef
Hongbo Zheng <hongboz2@illinois.edu>
2023-06-23 00:49:09 -0500

    update default config

--- src/config.rs
+++ src/config.rs
@@ -2,12 +2,12 @@ use crate::*;
 
 /* global variables */
 /// log level for the entire environment
-pub static LOG_LEVEL: LogLevel = LogLevel::Debug;
+pub static LOG_LEVEL: LogLevel = LogLevel::Info;
 /// percentage of max # of OS threads
 pub static mut THD_PCT: f64 = 0.80;
 /// maximum expression rewrite length
-pub static mut MAX_RW_LEN: u8 = 25;
+pub static mut MAX_RW_LEN: u8 = 35;
 /// exhaustive extraction flag
-pub static mut EXHAUSTIVE: bool = true;
+pub static mut EXHAUSTIVE: bool = false;
 /// suppress meaningless rewrite rules (e.g. * 1, pow 1, + 0)
 pub static SUPPRESS: bool = true;
