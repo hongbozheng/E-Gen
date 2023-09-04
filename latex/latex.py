61ef97e852c1718661e48dabb991b111cb226793
suyuan2 <suyuan2@illinois.edu>
2023-09-03 23:09:23 -0500

    second version of conversion code, add more math operators

--- latex/latex.py
+++ latex/latex.py
@@ -4,7 +4,7 @@ with open('input\input_exp.txt', 'r', encoding='utf-8') as f:
 output=[] #The output list which contains all latex expressions.
 
 
-num_input = {"+": 2, "-": 2, "*": 2, "/": 2, "pow": 2, "sqrt": 1}
+num_input = {"+": 2, "-": 2, "*": 2, "/": 2, "pow": 2, "sqrt": 1, "d": 2, "sin": 1, "cos": 1, "tan": 1, "asin": 1, "acos": 1, "atan":1 , "acot": 1, "asec": 1, "acsc": 1, "sinh": 1, "cosh": 1, "tanh": 1, "csch": 1, "sech": 1, "coth": 1, "ln": 1, "log": 2}
 
 
 def is_operator(token):
@@ -29,6 +29,48 @@ def token_to_latex(i, exp, token):
         latex_token = "(%s)" %(str(exp[i+1])) + "^{" + str(exp[i+2]) + "} " #latex form of power is "^{} "
     elif token == "sqrt":
         latex_token = "\sqrt{%s} " %(str(exp[i+1]))
+    elif token == "d":
+        latex_token = "\\frac{\mathrm{d}" + str(exp[i+2]) + "}{\mathrm{d}" + str(exp[i+1]) + "} "
+    elif token == "sin":
+        latex_token = "\sin(%s) " %(str(exp[i+1]))
+    elif token == "cos":
+        latex_token = "\cos(%s) " %(str(exp[i+1]))
+    elif token == "tan":
+        latex_token = "\tan(%s) " %(str(exp[i+1]))
+    elif token == "cot":
+        latex_token = "\cot(%s) " %(str(exp[i+1]))
+    elif token == "sec":
+        latex_token = "\sec(%s) " %(str(exp[i+1]))
+    elif token == "csc":
+        latex_token = "\csc(%s) " %(str(exp[i+1]))
+    elif token == "asin":
+        latex_token = "\\arcsin(%s) " %(str(exp[i+1]))
+    elif token == "acos":
+        latex_token = "\\arccos(%s) " %(str(exp[i+1]))
+    elif token == "atan":
+        latex_token = "\\arctan(%s) " %(str(exp[i+1]))
+    elif token == "acot":
+        latex_token = "\operatorname{arccot} (%s) " %(str(exp[i+1]))
+    elif token == "asec":
+        latex_token = "\operatorname{arcsec} (%s) " %(str(exp[i+1]))
+    elif token == "acsc":
+        latex_token = "\operatorname{arccsc} (%s) " %(str(exp[i+1]))
+    elif token == "sinh":
+        latex_token = "\sinh(%s) " %(str(exp[i+1]))
+    elif token == "cosh":
+        latex_token = "\cosh(%s) " %(str(exp[i+1]))
+    elif token == "tanh":
+        latex_token = "\tanh(%s) " %(str(exp[i+1]))
+    elif token == "coth":
+        latex_token = "\coth(%s) " %(str(exp[i+1]))
+    elif token == "csch":
+        latex_token = "\operatorname{csch} (%s) " %(str(exp[i+1]))
+    elif token == "sech":
+        latex_token = "\operatorname{sech} (%s) " %(str(exp[i+1]))
+    elif token == "ln":
+        latex_token = "\ln_{}{(%s)} " %(str(exp[i+1]))
+    elif token == "log":
+        latex_token = "\log_{%s}" %(str(exp[i+1])) + "{(%s)}" %(str(exp[i+2]))
     return latex_token
 
 def main(exp):
@@ -41,13 +83,13 @@ def main(exp):
         else:
             #print("current token is", exp[i])
             latex_token = token_to_latex(i, exp, exp[i])
-            print(latex_token)
+            #print(latex_token)
             num_token_del = num_input[exp[i]]
             del exp[i:i+num_token_del+1] #delete original token from the expression
-            print("before delete:", exp)
+            #print("before delete:", exp)
             exp.insert(i, latex_token) #replace the original token with the new latex form expression
             i-=1
-            print("after:", exp)
+            #print("after:", exp)
     return exp
 
 
