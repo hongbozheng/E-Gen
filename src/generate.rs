6d7f8f06e7a97d92495c005805feb234bbb44896
Hongbo Zheng <hongboz2@illinois.edu>
2023-09-29 17:15:24 -0500

    set up repo for bfs extraction

--- src/generate.rs
+++ src/generate.rs
@@ -1,6 +1,5 @@
 use crate::*;
 use bincode::{serialize, deserialize};
-use libc::{c_int, cpu_set_t, CPU_SET, pid_t, sched_setaffinity};
 use num_cpus;
 use serde::{Serialize, Deserialize};
 use std::collections::HashMap;
@@ -9,27 +8,6 @@ use std::io::{BufRead, BufReader, BufWriter, Read, Write};
 use std::mem::{size_of, zeroed};
 use std::net::TcpListener;
 use std::process::{Child, Command, exit};
-
-#[derive(Debug, Serialize, Deserialize)]
-/// data for extraction
-pub struct Data {
-    /// variable to store eclass(es) to skip during extraction
-    pub skip_ecls: HashMap<String, f64>,
-    /// variable to store grammar from MathEGraph
-    pub grammar: HashMap<String, Vec<String>>,
-}
-
-/// ### private unsafe function to set process affinity
-/// #### Arguments
-/// * `pid` - process id
-/// * `processor id` - processor id (CPU logic core id)
-/// #### Return
-/// * `c_int` - return 0 on success, return -1 on failure
-unsafe fn set_proc_affinity(pid: pid_t, processor_id: usize) -> c_int {
-    let mut cpuset: cpu_set_t = zeroed();
-    CPU_SET(processor_id, &mut cpuset);
-    sched_setaffinity(pid, size_of::<cpu_set_t>(), &cpuset)
-}
 
 /// ### private function generate equivalent expressions
 /// ### with 1 input expression
@@ -44,149 +22,10 @@ fn generate_exprs(cli: &mut Vec<CmdLineArg>) -> HashSet<String> {
     let mut ctx_gr = ContextGrammar::new(input_expr);
     ctx_gr.setup();
     let init_exprs = &ctx_gr.init_exprs.clone();
+    let CmdLineArg::UInt(levels) = &cli[2]
 
-    /* get number of processes */
-    let num_proc = init_exprs.len();
-
-    /* tx & rx listener */
-    let addr = "127.0.0.1:8080";
-    let listener = match TcpListener::bind(&addr) {
-        Ok(listener) => { listener },
-        Err(e) => {
-            log_error(&format!("[ERROR]: Failed to bind IP address \"{}\" with error {}.\n", addr, e));
-            exit(1);
-        },
-    };
-
-    /* insert socket address & get CPU's number of logical cores */
-    cli.push(CmdLineArg::String(addr.to_string()));
-    let num_logical_cores = num_cpus::get();
-
-    /* spawn children processes & set process affinity */
-    let mut child_procs: Vec<Child> = init_exprs.into_iter().zip(0..num_proc).map(|(rw, proc_idx)| {
-        cli[3] = CmdLineArg::String(rw.clone());
-
-        let args: Vec<String> = cli.iter().map(|arg| arg.to_string()).collect();
-
-        match Command::new("../target/debug/multiproc").args(&args).spawn() {
-            Ok(child_proc) => {
-                let pid = child_proc.id() as pid_t;
-                let processor_id = proc_idx % num_logical_cores;
-                let ret = unsafe { set_proc_affinity(pid, processor_id) };
-                match ret {
-                    0 => { log_debug(&format!("Set process {}'s process affinity to processor {}.\n", pid, processor_id)); },
-                    _ => {
-                        log_error(&format!("Failed to set process {}'s process affinity to processor {}.\n", pid, processor_id));
-                        exit(1);
-                    },
-                }
-                child_proc
-            },
-            Err(e) => {
-                log_error(&format!("Failed to spawn child process with error {}.\n", e));
-                exit(1);
-            },
-        }
-    }).collect();
-
-    let mut num_acks: u8 = 0u8;
-
-    /* send data to all children processes through sockets */
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let skip_ecls = ctx_gr.skip_eclasses.clone();
-                let grammar = ctx_gr.grammar.clone();
-
-                let data: Data = Data {
-                    skip_ecls,
-                    grammar,
-                };
-
-                let data_bytes = match serialize(&data) {
-                    Ok(data_bytes) => { data_bytes },
-                    Err(e) => {
-                        log_error(&format!("Failed to serialize data with error {}.\n", e));
-                        exit(1);
-                    },
-                };
-
-                match stream.write_all(&data_bytes) {
-                    Ok(_) => {
-                        num_acks += 1;
-                        log_debug(&format!("Data send to child process {:?} successfully.\n", stream.peer_addr()));
-                    },
-                    Err(e) => {
-                        log_error(&format!("Failed to send data to child process {:?} with error {}.\n", stream.peer_addr(), e));
-                        exit(1);
-                    },
-                }
-            }
-            Err(e) => {
-                log_error(&format!("Failed to connect to child process with error {}.\n", e));
-                exit(1);
-            }
-        }
-
-        if num_acks as usize == num_proc { break; }
-    }
-
-    num_acks = 0u8;
-    let mut equiv_exprs: HashSet<String> = Default::default();
-
-    /* receive equivalent expressions from all children processes */
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let mut equiv_exprs_bytes: Vec<u8> = vec![];
-                match stream.read_to_end(&mut equiv_exprs_bytes) {
-                    Ok(_) => {
-                        let equiv_exprs_proc: std::collections::HashSet<String> = match deserialize(&equiv_exprs_bytes) {
-                            Ok(equiv_exprs_proc) => { equiv_exprs_proc },
-                            Err(e) => {
-                                log_error(&format!("Failed to deserialize data received from child process with error {}.\n", e));
-                                exit(1);
-                            },
-                        };
-                        let ex: HashSet<String> = equiv_exprs_proc.into_iter().collect();
-                        equiv_exprs.extend(ex.into_iter());
-                        // equiv_exprs.append(&mut equiv_exprs_proc);
-                        num_acks += 1;
-                    },
-                    Err(e) => {
-                        log_error(&format!("Failed to receive data from child process socket address {:?} with error {}.\n", stream.peer_addr(), e));
-                        exit(1);
-                    },
-                }
-            }
-            Err(e) => {
-                log_error(&format!("Failed to connect to child process with error {}.\n", e));
-                exit(1);
-            }
-        }
-        if num_acks as usize == num_proc {
-            drop(listener);
-            break;
-        }
-    }
-
-    /* check if all children processes exit successfully */
-    for child_proc in &mut child_procs {
-        let pid = child_proc.id();
-        match child_proc.wait() {
-            Ok(exit_status) => {
-                match exit_status.code() {
-                    Some(0) => { log_debug(&format!("Child process {} terminated successfully with an exit code 0.\n", pid)); },
-                    Some(exit_code) => { log_error(&format!("Child process {} terminated with a non-zero exit code {}.\n", pid, exit_code)); },
-                    None => { log_error(&format!("Child process {} terminated with an unknown exit code.\n", pid)); },
-                }
-            },
-            Err(e) => { log_error(&format!("Child process {} is not running with error {}.\n", pid, e)); },
-        }
-    }
-
-    /* post-processing equivalent expressions */
-    equiv_exprs = rm_permutation(&equiv_exprs);
+    let mut equiv_exprs: HashSet<String> = HashSet::default();
+    equiv_exprs = bfs_ext(levels, init_exprs);
 
     return equiv_exprs;
 }
--- src/multiproc_ext.rs
+++ src/multiproc_ext.rs
@@ -1,8 +1,11 @@
 use std::env;
-use egg::extract;
+// use egg::extract;
 
+/**
+ * For multiprocessing purpose (NOT FOR SINGLE PROCESS BFS)
+ */
 fn main() {
     let args: Vec<String> = env::args().collect();
-    extract(&args);
+    // extract(&args);
     return;
 }
