639edbcf3f8862a0eac18ed23d1391fe9bea6aa2
Hongbo Zheng <hongboz2@illinois.edu>
2023-10-25 10:42:35 -0500

    change mathegraph to mutable ref

--- src/math.rs
+++ src/math.rs
@@ -66,7 +66,7 @@ pub struct ConstantFold;
 impl Analysis<Math> for ConstantFold {
     type Data = Option<(Constant, PatternAst<Math>)>;
 
-    fn make(egraph: &MathEGraph, enode: &Math) -> Self::Data {
+    fn make(egraph: &mut MathEGraph, enode: &Math) -> Self::Data {
         let x = |i: &Id| egraph[*i].data.as_ref().map(|d| d.0);
         Some(match enode {
             Math::Constant(c) => (*c, format!("{}", c).parse().unwrap()),
