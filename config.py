f20b665c203b454f48861b08b440d6f86f9ecd4e
Hongbo Zheng <hongboz2@illinois.edu>
2024-01-02 16:26:09 -0600

    update config.py

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
 import logger
 
-log_level = logger.LogLevel.info
+CLASSES = ["poly", "ln",
+           "sin", "cos", "tan",
+           "csc", "sec", "cot",
+           "asin", "acos", "atan",
+           "acsc", "asec", "acot",
+           "sinh", "cosh", "tanh",
+           "csch", "sech", "coth",
+           "asinh", "acosh", "atanh",
+           "acsch", "asech", "acosh",]
+CATEGORIES = ["general", "d",]
 
-operator = {'Add':'+',
-            'Sub':'-',
-            'Mul':'*',
-            'Div':'/'}
-operator_list = operator.values()
-var_list = ['x','y']
+DATA_DIR = "data"
+DATA_RAW_DIR = DATA_DIR + "/raw"
+DATA_PROCESSED_DIR = DATA_DIR + "/processed"
+
+LOG_LEVEL = logger.LogLevel.INFO
