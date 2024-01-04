cea23481035e16e975063a45565d59bc1a8aebb5
Hongbo Zheng <hongboz2@illinois.edu>
2024-01-03 23:47:23 -0600

    add filepaths to config

--- config.py
+++ config.py
@@ -15,4 +15,8 @@ DATA_DIR = "data"
 DATA_RAW_DIR = DATA_DIR + "/raw"
 DATA_PROCESSED_DIR = DATA_DIR + "/processed"
 
+EXPRS_FILEPATH = DATA_DIR + "/exprs.txt"
+EQUIV_EXPRS_FILEPATH = DATA_DIR + "/equiv_exprs.txt"
+DUPLICATES_FILEPATH = DATA_DIR + "/duplicates.txt"
+
 LOG_LEVEL = logger.LogLevel.INFO
