2f42da5bf65fdcf996442eb522447535b743cfc2
Hongbo Zheng <hongboz2@illinois.edu>
2024-01-03 23:48:46 -0600

    rm cli

--- create_raw_dataset.py
+++ create_raw_dataset.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 
 
-import argparse
 import config
-import glob
 import logger
 import os
-import tqdm
 
 
 def classify(expr: str, classes: list[str], categories: list[str]) -> tuple[str, str]:
@@ -42,55 +39,42 @@ def w_raw_data(data_raw_dir: str, equiv_exprs: list[str], cls: str, category: st
     return
 
 
-def create_raw_dataset(equiv_exprs_dir: str, classes: list[str], categories: list[str], data_raw_dir: str) -> None:
-    pathname = os.path.join(equiv_exprs_dir, "equiv_exprs_*.txt")
-    filepaths = glob.glob(pathname=pathname)
+def create_raw_dataset(equiv_exprs_filepath: str, classes: list[str], categories: list[str], data_raw_dir: str) -> None:
+    equiv_exprs_file = open(file=equiv_exprs_filepath, mode='r')
 
-    progbar = tqdm.tqdm(iterable=filepaths)
+    equiv_exprs = []
 
-    for filepath in progbar:
-        progbar.set_description(desc="[INFO]: Processing file '%s'" % filepath, refresh=True)
+    for line in equiv_exprs_file:
+        if line.strip() and line not in equiv_exprs:
+            equiv_exprs.append(line)
+        elif not line.strip():
+            equiv_exprs.append(line)
 
-        with open(file=filepath, mode='r') as file:
+            cls, category = classify(expr=equiv_exprs[0], classes=classes, categories=categories)
+            w_raw_data(data_raw_dir=data_raw_dir, equiv_exprs=equiv_exprs, cls=cls, category=category)
+
             equiv_exprs = []
 
-            for line in file:
-                if line.strip() and line not in equiv_exprs:
-                    equiv_exprs.append(line)
-                elif not line.strip():
-                    equiv_exprs.append(line)
-
-                    cls, category = classify(expr=equiv_exprs[0], classes=classes, categories=categories)
-                    w_raw_data(data_raw_dir=data_raw_dir, equiv_exprs=equiv_exprs, cls=cls, category=category)
-
-                    equiv_exprs = []
+    equiv_exprs_file.close()
 
     return
 
 
 def main() -> None:
+    if not os.path.exists(path=config.EQUIV_EXPRS_FILEPATH):
+        logger.log_error(f"'{config.EQUIV_EXPRS_FILEPATH}' file does not exist!")
+        logger.log_error(f"Make sure to run './deduplicate.py' first to create {config.EQUIV_EXPRS_FILEPATH} file.")
+        logger.log_error("Operation aborted.")
+        exit(1)
     if os.path.exists(path=config.DATA_RAW_DIR):
-        logger.log_info(f"Raw dataset directory '{config.DATA_RAW_DIR}' already exists!")
-        logger.log_info("Make sure you ONLY include NEWLY generated equivalent expressions .txt files in the directory "
-                        "you provided as command line input!")
-        logger.log_info("Or make sure you remove the existing raw dataset directory and include all generated "
-                        "equivalent expressions .txt files in the directory you provided as command line input!")
-
-        usr_input = input("[INFO]: Do you want to abort the operation? [Y/n]")
-        if usr_input == 'Y':
-            logger.log_info("Operation aborted.")
-            return
-
-    parser = argparse.ArgumentParser(prog="create_raw_dataset",
-                                     description="split generated equivalent expressions into categories")
-    parser.add_argument("--equiv_exprs_dir", "-d", type=str, required=True, help="Equivalent expressions directory")
-
-    args = parser.parse_args()
-    equiv_exprs_dir = args.equiv_exprs_dir
+        logger.log_error(f"{config.DATA_RAW_DIR} directory already exists!")
+        logger.log_error(f"Make sure to delete {config.DATA_RAW_DIR} directory first.")
+        logger.log_error("Operation aborted.")
+        exit(1)
 
     logger.log_info("Creating raw dataset...")
-    create_raw_dataset(equiv_exprs_dir=equiv_exprs_dir, classes=config.CLASSES, categories=config.CATEGORIES,
-                       data_raw_dir=config.DATA_RAW_DIR)
+    create_raw_dataset(equiv_exprs_filepath=config.EQUIV_EXPRS_FILEPATH, classes=config.CLASSES,
+                       categories=config.CATEGORIES, data_raw_dir=config.DATA_RAW_DIR)
     logger.log_info("Finish creating raw dataset.")
 
     return
